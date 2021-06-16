8e580416b981b7b843857bc68691849cf74ac650
Scot <bscot.hagen@gmail.com>
2021-06-16 14:55:30 -0700

    Add project files.

---
+++ RazorPagesMovie18/Data/RazorPagesMovie18Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie18.Models;
+
+namespace RazorPagesMovie18.Data
+{
+    public class RazorPagesMovie18Context : DbContext
+    {
+        public RazorPagesMovie18Context (DbContextOptions<RazorPagesMovie18Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie18.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie18/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie18.Data;
+using RazorPagesMovie18.Models;
+
+namespace RazorPagesMovie18.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie18.Data.RazorPagesMovie18Context _context;
+
+        public IndexModel(RazorPagesMovie18.Data.RazorPagesMovie18Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
