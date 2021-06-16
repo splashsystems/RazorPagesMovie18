db2d7bc72e10dd24a398c842db2f33af15214b1d
Scot <bscot.hagen@gmail.com>
2021-06-16 14:57:24 -0700

    Added Model

--- RazorPagesMovie18/Models/Movie.cs
+++ RazorPagesMovie18/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,10 @@ namespace RazorPagesMovie18.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
         public decimal Price { get; set; }
 
     }
